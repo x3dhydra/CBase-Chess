78fe5011d7f7bef9d997017e2bc3f697e24aee64
Austen Green <x3dhydra@gmail.com>
2011-11-27 22:02:35 -0600

    Initial Commit

---
+++ ChessUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChessUI
+//
+//  Created by Austen Green on 11/27/11.
+//  Copyright (c) 2011 Austen Green Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChessUI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChessUI
+//
+//  Created by Austen Green on 11/27/11.
+//  Copyright (c) 2011 Austen Green Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
